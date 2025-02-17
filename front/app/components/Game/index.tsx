b7408afc95a4d00ec955f6ac9efac21857e4382b
Marcelo Henrique Aguiar Marques <marcelohm@gmail.com>
2025-02-16 23:05:52 -0300

    Fate: Mecanica em loop inicial

--- front/app/components/Game/index.tsx
+++ front/app/components/Game/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useApp } from "@/app/context";
 import { 
     MainContainer, 
@@ -11,6 +11,12 @@ import {
 } from "@/app/styles/game";
 
 export default function Game() {
+  type Dot = {
+    id: number;
+    value: number;
+    createdAt: number; // Timestamp de criação
+  };
+
   const { 
     playSound,
     level,
@@ -21,6 +27,31 @@ export default function Game() {
     setLife
   } = useApp();
   const [ load, setLoad ] = useState(true);
+  const [dots, setDots] = useState<Dot[]>([]);
+
+  useEffect(() => {
+    const addDotInterval = setInterval(() => {
+      setDots((prevDots) => [
+        ...prevDots,
+        { id: Date.now(), value: Math.floor(Math.random() * 101), createdAt: Date.now() }
+      ]);
+    }, 5000);
+
+    const removeOldDotsInterval = setInterval(() => {
+      setDots((prevDots) =>
+        prevDots.filter((dot) => Date.now() - dot.createdAt < 30000) // Remove após 30s
+      );
+    }, 1000); // Verifica a cada 1s
+
+    return () => {
+      clearInterval(addDotInterval);
+      clearInterval(removeOldDotsInterval);
+    };
+  }, []);
+
+  const handleRemoveDot = (id: number) => {
+    setDots((prevDots) => prevDots.filter((dot) => dot.id !== id));
+  };
   
   return (
     <MainContainer>
@@ -40,10 +71,13 @@ export default function Game() {
             </TopSection>
             
             <MainSection>
-                <NumberDot  onClick={() => playSound("/audio/click.wav")}>
-                    4
-                </NumberDot>
+            {dots.map((dot) => (
+              <NumberDot key={dot.id} onClick={() => handleRemoveDot(dot.id)}>
+                {dot.value}
+              </NumberDot>
+            ))}
             </MainSection>
+            
             
             <BottomSection>
                 <div>
